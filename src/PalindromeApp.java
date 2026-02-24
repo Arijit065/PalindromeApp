53d8d471fa6ed5848aeb414ed0823ba3691c8439
Arijit065 <arijitmandal065@gmail.com>
2026-02-24 09:57:06 +0530

    uc1

--- src/PalindromeApp.java
+++ src/PalindromeApp.java
@@ -1,2 +1,8 @@
+
 public class PalindromeApp {
+ public static void main(String[] args) {
+     System.out.println("hello world");
+     System.out.println("Version 1");
+     System.out.println("System intialiastion");
+    }
 }
